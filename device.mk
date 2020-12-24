299e5396264994abbcdb1048ea79eac9c6ce6796
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-12-24 16:18:14 +0100

    m51: Build fastcharge HAL
    * Meanwhile import sepolicy for it
    
    Change-Id: Iec1a7faec24f0d5b4b1d2f1ef52b781368c5bd51

--- device.mk
+++ device.mk
@@ -193,6 +193,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd \
 
+# Fastcharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.samsung \
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.face@1.0.vendor \
