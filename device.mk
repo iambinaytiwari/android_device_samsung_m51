354b1906d1aaf459e518db7a95d455d51e6ab3c5
dianlujitao <dianlujitao@lineageos.org>
2023-08-11 15:31:16 +0200

    m51: Build the QTI perf libs from source
    Change-Id: Ic3a7450944651d4d2a47175c139941b9e2e1cb31

--- device.mk
+++ device.mk
@@ -419,6 +419,11 @@ PRODUCT_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
     $(DEVICE_PATH)/overlay-lineage \
 
+# Performance
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2 \
+    vendor.qti.hardware.perf@2.2.vendor \
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
