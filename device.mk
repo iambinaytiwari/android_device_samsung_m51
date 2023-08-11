6e9ba4960983143512cf00bcec637587af0b31d0
Andreas Huber <andih@google.com>
2023-08-11 15:44:05 +0200

    m51: Build the sensors NDK bridge
     * This is a "reverse" shim that implements the ASensor* NDK API using
       android.frameworks.sensorservice@1.0. The library is intended for use
       by vendor code, to enable easier transition from /dev/binder to
       /dev/hwbinder.  With this library, vendor code should be able to compile
       without source changes, simply by replacing libandroid.so with
       libsensorndkbridge.so.
    
     * At present the only binary left that still uses this
       library is the QTI slim daemon.
    
    Change-Id: Ib4e7c7eba72b403a2a0aed5eedf3c37c62a199b8

--- device.mk
+++ device.mk
@@ -580,6 +580,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
+    libsensorndkbridge \
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/vendor/etc/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
